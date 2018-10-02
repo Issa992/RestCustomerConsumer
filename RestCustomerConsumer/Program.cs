3b519dc4db3dd6acbba668ead249c1719dee23a1
Issa <abdu2997@edu.easj.dk>
2018-10-02 17:08:30 +0200

    GetAsyncAllCustomers

--- RestCustomerConsumer/Program.cs
+++ RestCustomerConsumer/Program.cs
@@ -1,21 +1,59 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RestCustomerConsumer
 {
     class Program
     {
+        private static string CustomerUri = "https://localhost:44316/api/Customers";
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+
+        }
+
+        static async Task RunAsync()
+        {
+            IList<Customer> CustomerList;
+            CustomerList = await GetCustomerAsync();
+            ShowCustomer(CustomerList);
+        }
+        static async Task ShowCustomer(IList<Customer> CustomerList)
+        {
+
+            foreach (var List in CustomerList)
+            {
+                Console.WriteLine($"Id:{List.Id}//////Name:{List.FirstName}{List.LastName}//////Year{List.Year}");
+
+            }
+        }
+
+        public static async Task<IList<Customer>> GetCustomerAsync()
+        {
+            using (HttpClient client =new HttpClient())
+            {
+                string content = await client.GetStringAsync(CustomerUri);
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
         }
     }
 }
